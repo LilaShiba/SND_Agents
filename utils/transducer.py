e68ba1061906291b3ebcfa738fbece2c94a968e6
kyle1james <lilaresearch@gmail.com>
2023-11-25 10:35:44 -0500

    metrics clean up

--- utils/transducer.py
+++ utils/transducer.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from typing import *
 from utils.agent import Agent
 from utils.nets import Neuron
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 class Transducer:
@@ -20,27 +20,33 @@ class Transducer:
         Create bounded plane of responses for agent_a & agent_b
         '''
         # init
-        self.agents = defaultdict()
-        self.jaccard_indices = defaultdict()
-        self.prob_vectors = defaultdict()
-        self.r = defaultdict(int)
+        self.agents = {agent.name: agent for agent in agents}
+
+        self.jaccard_indices = defaultdict(float)
+        self.prob_vectors = defaultdict(list)
+        self.all_words_count = defaultdict(int)
         self.all_responses: str = ""
         self.n: int
         self.neurons = defaultdict()
         # set values
+
         for agent in agents:
             if question != "":
                 print('loading agent', agent.name, '... ')
                 agent.chat_bot.one_question(question)
-
-            self.agents[agent.name] = agent
             self.all_responses += agent.response
 
-            for word in agent.response.split():
-                self.r[word.lower()] += 1
-        self.all_responses = set(self.all_responses.split())
+        self.all_responses = self.all_responses.split()
         self.n = len(self.all_responses)
-        self.bounded_space()
+        print(self.all_responses)
+        self.agent_response = {agent.name: agent.response.split()
+                               for agent in agents}
+        self.all_words_count = self._get_word_freq(self.all_responses)
+        self.all_words_prob = self._get_prob_vect(self.all_responses)
+        self.prob_vectors = {agent.name: [
+            self._get_prob_vect(agent.response)] for agent in agents}
+
+        # self.bounded_space()
 
     def bounded_space(self) -> None:
         '''
@@ -52,14 +58,11 @@ class Transducer:
                 the one-hot encoded vector of word frequencies.
         '''
         # Splitting responses into words and creating sets
-
         for name, agent in self.agents.items():
-            response = set(agent.response.split())
-            print('response', response)
+            response = agent.response.split()
             # Calculating the union and intersection of word sets
             union_words = response.union(self.all_responses)
-            intersection_words = response.intersection(
-                self.all_responses)
+            intersection_words = response.intersection(self.all_responses)
 
             # Calculating the Jaccard index
             jaccard_index = len(intersection_words) / \
@@ -67,26 +70,56 @@ class Transducer:
 
             self.jaccard_indices[name] = jaccard_index
 
+            # for name_2, agent_2 in self.agents.items():
+            #     if name_2 != name:
+            #         union_words = response.union(agent_2.response)
+            #         intersection_words = response.intersection(self.all_responses)
+
+            #         # Calculating the Jaccard index
+            #         jaccard_index = len(intersection_words) / \
+            #             len(union_words) if union_words else 0
+
+            #         self.prob_vectors[(name, name_2)] =
+            #         self.jaccard_indices[(name, name_2)] =
+
             # Counting occurrences of each word in the combined responses
-            self.prob_vectors[name] = self.prob_vect(response)
+            # self.prob_vectors[name] = self.prob_vect(response)
 
-    def prob_vect(self, response: str) -> np.array:
+    def _get_prob_vect(self, response: str):
         '''
-        returns dict of probs of words
+        input:
+        set of response string
+        returns:
+        prob-vector: (np.array) word_count / total_response_words
+        hot-coded: (np.array) word_count / total_same_word_in_all_responses
         '''
-        prob_vect = [0] * len(self.r)
-        word_freq = self.word_freq(response)
-        idx = 0
-        for key, _ in self.r.items():
-            prob_vect[idx] = word_freq[key.lower()] / len(word_freq)
-            idx += 1
+        if isinstance(response, str):
+            response = response.split()
 
-        return prob_vect
+        agent_w_count = self._get_word_freq(response)
+        hotcode_vector = [0] * len(self.all_responses)
+        probabilty_vector = [0] * len(agent_w_count)
+        hotcode_vector = np.array(
+            [agent_w_count[key.lower()] / self.n for key in self.all_responses])
+        probabilty_vector = np.array([agent_w_count[key.lower(
+        )] / self.all_words_count[key.lower()] for key in self.all_responses])
+
+        return [probabilty_vector, hotcode_vector]
 
         # for key, value in word_freq_dict:
         #     prob_vect[key] = value / self.n
 
-    def word_freq(self, pasage: str) -> dict:
+    def _get_jaccard(self, agent):
+        response = agent.response.split()
+        union_words = response.union(self.all_responses)
+        intersection_words = response.intersection(self.all_responses)
+
+        # Calculating the Jaccard index
+        jaccard_index = len(intersection_words) / \
+            len(union_words) if union_words else 0
+        self.jaccard_indices[agent.name] = jaccard_index
+
+    def _get_word_freq(self, pasage: str) -> dict:
         '''
         returns a freq dict of words in passage
         '''
@@ -107,8 +140,8 @@ class Transducer:
         Returns:
         float: Shannon Entropy of the dataset.
         """
-        counts = self.prob_vect(response)
-        return -sum(p * np.log(p) if p > 0 else 0 for p in counts)
+        counts = Counter(response)
+        return -sum(p * np.log(p) if p > 0 else 0 for p in counts.values())
 
     def true_diversity(self, response: str) -> float:
         """
@@ -120,23 +153,24 @@ class Transducer:
         Returns:
         float: True Diversity of the dataset.
         """
-        counts = self.word_freq(response).values()
-        return np.exp(self.shannon_entropy(counts))
+        return np.exp(self.shannon_entropy(response))
 
     def create_layer(self) -> np.array:
         '''
         creates transducer layer for input
         '''
 
-        main_vector = self.prob_vect(self.all_responses)
+        main_vector = self.all_words_prob
         idx = 0
         for name, agent in self.agents.items():
-            prob_v = np.array(self.prob_vectors[name])
+            # hot key-encode vector
+            prob_v = self.prob_vectors[name][0][1]
             delta_dot = np.dot(main_vector, prob_v)
-            self.neurons[name] = Neuron(input=delta_dot, id=idx, layer=1)
-            self.neurons[name].prov_v = prob_v
+            print(f'delta dot product {delta_dot}')
+            self.neurons[name] = Neuron(input=delta_dot[1], id=idx, layer=1)
+            self.neurons[name].probabilty_vector = prob_v
             self.neurons[name].dot_product = delta_dot
-            self.neurons[name].cross = main_vector * prob_v
+            self.neurons[name].cross_product = main_vector * prob_v
             self.neurons[name].jaccard_index = self.jaccard_indices[name]
             self.neurons[name].x = self.jaccard_indices[name]
             self.neurons[name].y = delta_dot
